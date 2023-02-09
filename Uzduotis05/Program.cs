f8e032ae22fe65fe6fe34ac66df45ff06389f08c
ErikaVJ <zutyte@yahoo.co.uk>
2023-02-09 22:02:30 +0200

    Start task 5

---
+++ Uzduotis05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzduotis05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Išveskite į ekraną dviejų skaičių sumą, skirtumą, sandaugą ir dalmenį,
+             * kiekvieną kartą nurodant atliekamą veiksmą ir šių skaičių reikšmes.
+             * Pvz.: 
+             * 4 + 5 = 9
+             * 4 - 5 = -1
+             * …
+             */
+
+        }
+    }
+}
